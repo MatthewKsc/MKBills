8b269da5feabfcfac43340ba117b9d27d055ded1
MatthewKsc <ksciukmateusz@gmail.com>
2020-04-28 12:04:56 +0200

    Initial commit

---
+++ bill-logic/src/main/java/com/matthewksc/billlogic/BillLogicApplication.java
@@ -0,0 +1,13 @@
+package com.matthewksc.billlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillLogicApplication.class, args);
+	}
+
+}
---
+++ bill-logic/src/test/java/com/matthewksc/billlogic/BillLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewksc.billlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
