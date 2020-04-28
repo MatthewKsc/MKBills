eba31c55a86fe70dfa2f017a4f7cc29b777c9b1f
MatthewKsc <ksciukmateusz@gmail.com>
2020-04-28 12:32:39 +0200

    BillController

---
+++ bill-logic/src/main/java/com/matthewksc/billlogic/BillController.java
@@ -0,0 +1,43 @@
+package com.matthewksc.billlogic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class BillController {
+
+    private BillService billService;
+
+    @Autowired
+    public BillController(BillService billService) {
+        this.billService = billService;
+    }
+
+    @GetMapping
+    public Iterable<Bill> getAll(){
+        return billService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Bill> getById(@RequestParam Long id){
+        return billService.findById(id);
+    }
+
+    @PostMapping("/design")
+    public Bill addBill(@RequestBody Bill bill){
+        return billService.save(bill);
+    }
+
+    @PutMapping("/update")
+    public Bill updateBill(@RequestBody Bill bill){
+        return billService.save(bill);
+    }
+
+    @DeleteMapping
+    public void deleteBill(Long id){
+        billService.delete(id);
+    }
+}
