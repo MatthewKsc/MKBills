2f9bfa1f00bfe0c23113d59095693157ac96a7d5
MatthewKsc <ksciukmateusz@gmail.com>
2020-05-06 18:02:37 +0200

    Added token and mail sender with implementation in register form

--- bill-logic/src/main/java/com/matthewksc/billlogic/Controllers/MainController.java
+++ bill-logic/src/main/java/com/matthewksc/billlogic/Controllers/MainController.java
@@ -1,20 +1,28 @@
 package com.matthewksc.billlogic.Controllers;
 
 
+import com.matthewksc.billlogic.Dao.TokenRepository;
+import com.matthewksc.billlogic.Dao.UserRepository;
+import com.matthewksc.billlogic.Dao.entity.Token;
 import com.matthewksc.billlogic.Dao.entity.User;
 import com.matthewksc.billlogic.Services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     private UserService userService;
+    private TokenRepository tokenRepository;
+    private UserRepository userRepository;
 
-    public MainController(UserService userService) {
+    public MainController(UserService userService, TokenRepository tokenRepository, UserRepository userRepository) {
         this.userService = userService;
+        this.tokenRepository = tokenRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/sing-up")
@@ -25,8 +33,17 @@ public class MainController {
 
     @PostMapping("/register")
     public String register(User user){
-        System.out.println(user);
+        userService.addUser(user);
         return "sing-up";
+    }
+
+    @GetMapping("/token")
+    public String singUp(@RequestParam String value){
+        Token byValue = tokenRepository.findTokeByValue(value);
+        User user = byValue.getUser();
+        user.setEnable(true);
+        userRepository.save(user);
+        return "hello";
     }
 
 
--- bill-logic/src/main/java/com/matthewksc/billlogic/Services/UserService.java
+++ bill-logic/src/main/java/com/matthewksc/billlogic/Services/UserService.java
@@ -3,9 +3,13 @@ package com.matthewksc.billlogic.Services;
 import com.matthewksc.billlogic.Dao.TokenRepository;
 import com.matthewksc.billlogic.Dao.UserRepository;
 import com.matthewksc.billlogic.Dao.entity.Role;
+import com.matthewksc.billlogic.Dao.entity.Token;
 import com.matthewksc.billlogic.Dao.entity.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -15,14 +19,32 @@ public class UserService {
     private PasswordEncoder passwordEncoder;
     private MailService mailService;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, TokenRepository tokenRepository,
+                       PasswordEncoder passwordEncoder, MailService mailService) {
         this.userRepository = userRepository;
+        this.tokenRepository = tokenRepository;
         this.passwordEncoder = passwordEncoder;
+        this.mailService = mailService;
     }
 
     public void addUser(User user){
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole(Role.ROLE_USER);
         userRepository.save(user);
+        sendToken(user);
+    }
+
+    public void sendToken(User user){
+        String tokeValue = UUID.randomUUID().toString();
+        Token token = new Token();
+        token.setUser(user);
+        token.setValue(tokeValue);
+        tokenRepository.save(token);
+        String url = "http://localhost:8080/token?value=" + tokeValue;
+        try {
+            mailService.sendMail(user.getEmail(), "Submit registration",url, false);
+        }catch (MessagingException e){
+            e.printStackTrace();
+        }
     }
 }
