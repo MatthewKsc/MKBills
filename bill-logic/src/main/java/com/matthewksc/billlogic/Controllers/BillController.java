48e417db43971dbb446b920c6bcda0ef3cdff931
MatthewKsc <ksciukmateusz@gmail.com>
2020-05-07 16:47:23 +0200

    Added new methods to UserService and replace some methods in BillController
    Signed-off-by: MatthewKsc <ksciukmateusz@gmail.com>

--- bill-logic/src/main/java/com/matthewksc/billlogic/Controllers/BillController.java
+++ bill-logic/src/main/java/com/matthewksc/billlogic/Controllers/BillController.java
@@ -3,6 +3,7 @@ package com.matthewksc.billlogic.Controllers;
 import com.matthewksc.billlogic.Dao.UserRepository;
 import com.matthewksc.billlogic.Dao.entity.Bill;
 import com.matthewksc.billlogic.Services.BillService;
+import com.matthewksc.billlogic.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,27 +11,27 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/bill")
+@RequestMapping("/user/bills")
 public class BillController {
 
     private BillService billService;
-    private UserRepository userRepository;
+    private UserService userService;
 
     @Autowired
-    public BillController(BillService billService, UserRepository userRepository) {
+    public BillController(BillService billService, UserService userService) {
         this.billService = billService;
-        this.userRepository = userRepository;
+        this.userService = userService;
     }
 
-    @GetMapping
-    public Iterable<Bill> getAll(){
-        return billService.findAll();
+    @GetMapping("/{userId}/{billId}")
+    public Optional<Optional<Bill>> getBill(@PathVariable Long userId, @PathVariable Long billId){
+        return userService.getUserBill(userId, billId);
     }
 
-//    @GetMapping("/{id}")
-//    public Optional<Bill> getById(@PathVariable Long id){
-//        return billService.findById(id);
-//    }
+    @GetMapping("/{userId}")
+    public Optional<List<Bill>> userBills(@PathVariable Long userId){
+        return userService.getAllUserBills(userId);
+    }
 
     @PostMapping("/design")
     public Bill addBill(@RequestBody Bill bill){
@@ -45,11 +46,5 @@ public class BillController {
     @DeleteMapping
     public void deleteBill(Long id){
         billService.delete(id);
-    }
-
-    //for now listing bills for user
-    @GetMapping("/{username}")
-    public Optional<List<Bill>> userBills(@PathVariable String username){
-        return userRepository.findByUsername(username).map(user -> user.getBills());
     }
 }
--- bill-logic/src/main/java/com/matthewksc/billlogic/Services/UserService.java
+++ bill-logic/src/main/java/com/matthewksc/billlogic/Services/UserService.java
@@ -1,5 +1,6 @@
 package com.matthewksc.billlogic.Services;
 
+import com.matthewksc.billlogic.Dao.BillRepository;
 import com.matthewksc.billlogic.Dao.TokenRepository;
 import com.matthewksc.billlogic.Dao.UserRepository;
 import com.matthewksc.billlogic.Dao.entity.Bill;
@@ -10,19 +11,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class UserService {
 
+    private BillRepository billRepository;
     private UserRepository userRepository;
     private TokenRepository tokenRepository;
     private PasswordEncoder passwordEncoder;
     private MailService mailService;
 
-    public UserService(UserRepository userRepository, TokenRepository tokenRepository,
+    public UserService(BillRepository billRepository, UserRepository userRepository, TokenRepository tokenRepository,
                        PasswordEncoder passwordEncoder, MailService mailService) {
+        this.billRepository = billRepository;
         this.userRepository = userRepository;
         this.tokenRepository = tokenRepository;
         this.passwordEncoder = passwordEncoder;
@@ -48,5 +52,14 @@ public class UserService {
         }catch (MessagingException e){
             e.printStackTrace();
         }
+    }
+    // return one bill by id
+    public Optional<Optional<Bill>> getUserBill(Long userId, Long billId){
+        return userRepository.findById(userId).map(user -> {
+            return billRepository.findById(billId);});
+    }
+    //returning all bills of specific user
+    public Optional<List<Bill>> getAllUserBills(Long userId){
+        return userRepository.findById(userId).map(user -> user.getBills());
     }
 }
