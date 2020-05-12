1dcead249d72fd62871374868790d828f176a151
MatthewKsc <ksciukmateusz@gmail.com>
2020-05-12 17:04:53 +0200

    impl of getUserBill test
    Signed-off-by: MatthewKsc <ksciukmateusz@gmail.com>

--- bill-logic/src/test/java/com/matthewksc/billlogic/Services/UserServiceTest.java
+++ bill-logic/src/test/java/com/matthewksc/billlogic/Services/UserServiceTest.java
@@ -4,6 +4,7 @@ import com.matthewksc.billlogic.Dao.BillRepository;
 import com.matthewksc.billlogic.Dao.UserRepository;
 import com.matthewksc.billlogic.Dao.entity.Bill;
 import com.matthewksc.billlogic.Dao.entity.User;
+import com.matthewksc.billlogic.Exeptions.NotFoundBillException;
 import com.matthewksc.billlogic.Exeptions.NotFoundUserException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,7 +24,7 @@ public class UserServiceTest {
     @Mock
     UserRepository userRepository;
     @Mock
-    BillRepository billRepository;
+    BillService billService;
 
     @InjectMocks
     UserService userService;
@@ -34,6 +35,24 @@ public class UserServiceTest {
 
     @Test
     public void getUserBill() {
+        User user = initData();
+        Bill bill = user.getBills()
+                .parallelStream()
+                .findFirst()
+                .get();
+        Bill otherBill = new Bill();
+        given(billService.findById(2L)).willReturn(bill);
+        given(billService.findById(1L)).willThrow(NotFoundBillException.class);
+        given(userRepository.findById(1L)).willReturn(java.util.Optional.of(user));
+
+        Bill check = userService.getUserBill(1L, 2L);
+
+        //check if getUserBill retuning good bill
+        assertEquals(bill, check);
+        assertNotEquals(otherBill,check);
+        //checks if exceptions work fine
+        assertThrows(NotFoundUserException.class,() -> userService.getUserBill(2L, 2L));
+        assertThrows(NotFoundBillException.class,() -> userService.getUserBill(1L, 1L));
     }
 
     @Test
@@ -49,8 +68,10 @@ public class UserServiceTest {
         //noElementList check that each user have his one list of bills
         assertEquals(noElementsList.size(), 0);
         assertNotEquals(noElementsList.size(), 2);
+        //check size of list
         assertEquals(billList.size(), 3);
         assertNotEquals(billList.size(), 2);
+        //checking if notFoundUserException works properly
         assertThrows(NotFoundUserException.class, ()->userService.getAllUserBills(3L));
     }
 
