4a21a0986dca03dcde52c825acf86b319a4c0c4c
MatthewKsc <ksciukmateusz@gmail.com>
2020-05-12 11:58:34 +0200

    init test
    Signed-off-by: MatthewKsc <ksciukmateusz@gmail.com>

---
+++ bill-logic/src/test/java/com/matthewksc/billlogic/Services/BillServiceTest.java
@@ -0,0 +1,25 @@
+package com.matthewksc.billlogic.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BillServiceTest {
+
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ bill-logic/src/test/java/com/matthewksc/billlogic/Services/UserServiceTest.java
@@ -0,0 +1,28 @@
+package com.matthewksc.billlogic.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void getUserBill() {
+    }
+
+    @Test
+    void getAllUserBills() {
+    }
+
+    @Test
+    void addUserBill() {
+    }
+
+    @Test
+    void save() {
+    }
+}
